8da2d8bc243f42aa48b18fe69c0671aa19152982
yoonjaee <hyoo97@naver.com>
2020-08-27 18:13:14 +0900

    dg

--- duo/ViewController.swift
+++ duo/ViewController.swift
@@ -7,14 +7,13 @@
 //
 import UIKit
 import Alamofire
-import NaverThirdPartyLogin
 import KakaoSDKAuth
 import KakaoSDKUser
 import GoogleSignIn
 import CoreData
 
-class ViewController: UIViewController, NaverThirdPartyLoginConnectionDelegate,GIDSignInDelegate, UITabBarControllerDelegate {
-   
+
+class ViewController: UIViewController,GIDSignInDelegate, UITabBarControllerDelegate {
     var loginlist : [NSManagedObject] = [] // 코어데이터에 로그인 정보 저장할 객체 배열 생성
     
     //데이터 저장함수
@@ -156,45 +155,6 @@ class ViewController: UIViewController, NaverThirdPartyLoginConnectionDelegate,G
         userLoginConfirm(idToken, idTokenExpire, rfToken, "google")
     }
     
-    // 1. 네이버 로그인
-    // -----------------------------------------------------------------------------------------------------------
-    let loginConn = NaverThirdPartyLoginConnection.getSharedInstance();
-    
-    // 네이버 로그인 버튼 클릭 시
-    @IBAction func naverSignIn(_ sender: UIButton) {
-        loginConn?.delegate = self;
-        loginConn?.requestThirdPartyLogin()
-    }
-    
-    // 로그인 했을 시 샐행됨
-    func getNaverEmailFromURL() {
-        // 받은 데이터를 이용해서 사용자 정보 가져오기
-        guard let ACToken = loginConn?.accessToken else {return}
-        guard let ACExpireDate = loginConn?.accessTokenExpireDate else {return}
-        guard let RFToken = loginConn?.refreshToken else {return}
-        
-        self.userLoginConfirm(ACToken, ACExpireDate, RFToken, "naver");
-    }
-    
-    
-    
-    // 로그인 성공했을 시, 로그인 토큰을 건네 받고 이 함수 실행됨.
-    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
-        getNaverEmailFromURL()
-    }
-    // 로그인 실패, 토큰이 없다.
-    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
-        // 틀렸다는 alert표시
-    }
-    // 접근 코드 갱신
-    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {}
-    func oauth20ConnectionDidFinishDeleteToken() {}
-    
-    // Access 토큰 갱신
-    func requestAccessTokenWithRefreshToken(){
-        oauth20ConnectionDidFinishRequestACTokenWithRefreshToken()
-    }
-    
     // 2. 카카오 로그인
     // -----------------------------------------------------------------------------------------------------------
     
@@ -248,10 +208,13 @@ class ViewController: UIViewController, NaverThirdPartyLoginConnectionDelegate,G
         }
     }
     
+    @IBAction func call_naverclass( sender: AnyObject) {
+        naver().naverSignIn()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         GIDSignIn.sharedInstance().delegate = self
-        resetAllRecords()
 //        if !isEmpty{
 //
 //        }
