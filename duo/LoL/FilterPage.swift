883d56a865d6c9a1284fba651cb5b2cd38819185
yoonjaee <hyoo97@naver.com>
2020-09-08 03:47:03 +0900

    almostdone

--- duo/LoL/FilterPage.swift
+++ duo/LoL/FilterPage.swift
@@ -11,46 +11,71 @@ import Alamofire
 
 class Filterpage : UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-   
-    @IBOutlet var GameModeButtons: [UIButton]!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        start.delegate = self; start.dataSource = self; end.delegate = self; end.dataSource = self
-//        solo.layer.cornerRadius = 10; free.layer.cornerRadius = 10; custom.layer.cornerRadius = 10
-//        urf.layer.cornerRadius = 10; random.layer.cornerRadius = 10
-//        top.layer.cornerRadius = 10; mid.layer.cornerRadius = 10; jungle.layer.cornerRadius = 10
-//        dealer.layer.cornerRadius = 10; support.layer.cornerRadius = 10
+        start.delegate = self
+        start.dataSource = self
+        end.delegate = self
+        end.dataSource = self
+        top.layer.cornerRadius = 10;
+        mid.layer.cornerRadius = 10;
+        jungle.layer.cornerRadius = 10;
+        dealer.layer.cornerRadius = 10;
+        support.layer.cornerRadius = 10;
     }
-
+    
+    //게임모드 버튼 누를시 색상변경
+    @IBOutlet var GameModeButtons: [UIButton]!
     @IBAction func ButtonSelected(_ sender: UIButton) {
         GameModeButtons.forEach({$0.backgroundColor = UIColor.white})
         sender.backgroundColor = UIColor.blue
         
     }
     
-    @IBAction func button_process(sender : UIButton){
-        if sender.backgroundColor == UIColor.white{
-            sender.backgroundColor == UIColor.blue
+    //게임모드 결정
+    
+    func getGameModeType() -> GameModeNumber {
+        for (index, button) in GameModeButtons.enumerated(){
+            if button.backgroundColor == UIColor.blue{
+                return GameModeNumber(rawValue: index) ?? .freeRank
+            }
         }
-        else if sender.backgroundColor == UIColor.blue{
-            sender.backgroundColor == UIColor.red
-        }
-        else{
-            sender.backgroundColor == UIColor.white
-        }
+        return .freeRank
     }
     
+    enum GameModeNumber : Int{
+        case soloRank
+        case freeRank
+        case normal
+        case knifeWind
+        case custom
+    }
+    
+    
+    // 포지션 버튼 누를시 색상변경
+    @IBAction func button_process(sender : UIButton){
+        if sender.backgroundColor == UIColor.white{
+            sender.backgroundColor = UIColor.blue
+        }
+        else if sender.backgroundColor == UIColor.blue{
+            sender.backgroundColor = UIColor.red
+        }
+        else{
+            sender.backgroundColor = UIColor.white
+        }
+       
+    }
+    
+    //인원수
     @IBOutlet weak var peoplenum: UILabel!
     var headcount : Int = 0
     @IBAction func sliderValueChanged(sender: UISlider) {
         var value = Int(sender.value) //UISlider(sender)의 value를 Int로 캐스팅해서 current라는 변수로 보낸다.
-        
         peoplenum.text = "\(value)"
         headcount = value
     }
     
-    
+    //티어 관련부분
     @IBOutlet weak var start: UIPickerView!
     @IBOutlet weak var end: UIPickerView!
     
@@ -68,18 +93,24 @@ class Filterpage : UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         return tier[row]
     }
     
+    //선택된 시간 String타입으로 변환
+    var time : String = ""
+    @IBAction func DatePicker (sender: UIDatePicker){
+        let datePickerView = sender
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        time = formatter.string(from: datePickerView.date)
+    }
+    
     @IBOutlet weak var top: UIButton!
     @IBOutlet weak var mid: UIButton!
     @IBOutlet weak var jungle: UIButton!
     @IBOutlet weak var dealer: UIButton!
     @IBOutlet weak var support: UIButton!
     
-    @IBAction func DatePicker (sender: UIDatePicker){
-        
-    }
-    
+    //포지션별 값
     var position = ["top":3,"mid":3, "jungle":3,"bottom":3,"support":3]
-    
+    //포지션 색상(선택여부)에따라 값 지정
     func positionresult(){
         if top.backgroundColor == UIColor.red{
             position["top"]=2
@@ -113,54 +144,48 @@ class Filterpage : UIViewController, UIPickerViewDelegate, UIPickerViewDataSourc
         }
     }
     
-    @IBAction func save(_ sender: UIButton) {
+    //적용버튼
+    @IBAction func apply(_ sender: UIButton) {
         
-        getPosts()
+        //        getPosts()
         dismiss(animated: true)
     }
     
-    func getGameModeType() -> GameModeNumber {
-        for (index, button) in GameModeButtons.enumerated(){
-            if button.backgroundColor == UIColor.red{
-                return GameModeNumber(rawValue: index) ?? .freeRank
-            }
+    //토크온 기능
+    var talkon = 3
+    @IBAction func `switch`(_ sender: UISwitch) {
+        if sender.isOn {
+            talkon = 1
+        } else {
+            talkon = 2
         }
-        return .freeRank
     }
     
-    enum GameModeNumber : Int{
-        case soloRank
-        case freeRank
-        case normal
-        case knifeWind
-        case custom
-    }
-
-    func getPosts() {
-        BaseFunc.fetch();
-        let url = URL(string : BaseFunc.baseurl + "/post/lol/getpost/filter")!
-        let req = AF.request(url,
-                            method:.post,
-                            parameters: ["gameMode": getGameModeType(),"wantTier": 17,"startTime": "17:00", "headcount": headcount,"top": position["top"],"mid":position["mid"],"jungle": position["jungle"],"bottom": position["bottom"],"support": position["support"],"talkon":3],
-                            encoding: JSONEncoding.default)
-        // db에서 값 가져오기
-        req.responseJSON {res in
-            switch res.result {
-            case.success(let value):
-
-                if let datas = value as? Array<Dictionary<String,Any>> {
-                    self.postsData = datas;
-
-                    DispatchQueue.main.async {
-                        // 테이블 뷰에 그리기
-                        self.TableViewController.reloadData();
-                    }
-                }
-
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-
+    //    func getPosts() {
+    //        BaseFunc.fetch();
+    //        let url = URL(string : BaseFunc.baseurl + "/post/lol/getpost/filter")!
+    //        let req = AF.request(url,
+    //                            method:.post,
+    //                            parameters: ["gameMode": getGameModeType(),"wantTier": 17,"startTime": time, "headcount": headcount,"top": position["top"],"mid":position["mid"],"jungle": position["jungle"],"bottom": position["bottom"],"support": position["support"],"talkon":talkon],
+    //                            encoding: JSONEncoding.default)
+    //        // db에서 값 가져오기
+    //        req.responseJSON {res in
+    //            switch res.result {
+    //            case.success(let value):
+    //
+    //                if let datas = value as? Array<Dictionary<String,Any>> {
+    //                    self.postsData = datas;
+    //
+    //                    DispatchQueue.main.async {
+    //                        // 테이블 뷰에 그리기
+    //                        self.TableViewController.reloadData();
+    //                    }
+    //                }
+    //
+    //            case .failure(let error):
+    //                print(error)
+    //            }
+    //        }
+    //    }
+    
 }
