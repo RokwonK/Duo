a57d7b185a42acc7b370ea5efe6e4a7cae16845a
helloyjae <hyoo97@naver.com>
2020-12-21 22:00:40 +0900

    .

--- duo/View/SelectBoard.swift
+++ duo/View/SelectBoard.swift
@@ -15,7 +15,6 @@ class SelectBoard : UIViewController, UITableViewDelegate, UITableViewDataSource
     @IBOutlet weak var uploadComment: UIButton!
     
     
-    
     let ad = UIApplication.shared.delegate as? AppDelegate
     var commentsData : Array<Dictionary<String, Any>>?;
     
