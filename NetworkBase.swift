17796918b92e7f66fc08f88af11157cf0396bade
RokwonK <wonrok97@naver.com>
2021-01-06 00:24:23 +0900

    Network base코드 작성중

---
+++ NetworkBase.swift
@@ -0,0 +1,19 @@
+//
+//  ApiBase.swift
+//  duo
+//
+//  Created by 김록원 on 2021/01/06.
+//  Copyright © 2021 김록원. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class NetworkBase : NSObject {
+    
+    func requestGet<T : Codable>(type : T.Type, success : ( () -> Void)?, failure : ( () -> Void )?) {
+        
+//        AF.request(<#T##convertible: URLConvertible##URLConvertible#>,method: <#T##HTTPMethod#>, parameters: <#T##Encodable?#>, encoder: <#T##ParameterEncoder#>, headers: <#T##HTTPHeaders?#>, interceptor: <#T##RequestInterceptor?#>, requestModifier: <#T##Session.RequestModifier?##Session.RequestModifier?##(inout URLRequest) throws -> Void#>)
+    }
+    
+}
