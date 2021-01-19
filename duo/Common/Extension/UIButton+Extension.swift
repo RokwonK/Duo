d36385cce3eee36c0440e75f21bd6b32ea5812eb
RokwonK <wonrok97@naver.com>
2021-01-20 01:06:03 +0900

    main 화면 교체중

--- duo/Common/Extension/UIButton+Extension.swift
+++ duo/Common/Extension/UIButton+Extension.swift
@@ -8,14 +8,16 @@
 
 import UIKit
 
-extension UIButton {
-    func setShadow(color : CGColor, width : Int, height : Int, opacity : Float, radius : CGFloat) {
+
+
+extension UIView {
+    func setShadow(color : CGColor, width : CGFloat, height : CGFloat, opacity : Float, radius : CGFloat) {
         
         self.layer.shadowColor = color
-        self.layer.shadowOffset = CGSize(width : width,height : height )
+        self.layer.shadowOffset = CGSize(width : width, height : height)
         self.layer.shadowOpacity = opacity
         self.layer.shadowRadius = radius
+//        self.layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
     }
-    
     
 }
--- duo/View/메인( 게임 선택창 )/HomeViewController.swift
+++ duo/View/메인( 게임 선택창 )/HomeViewController.swift
@@ -10,8 +10,29 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var lolView: UIView!
+    @IBOutlet weak var bagView: UIView!
+    @IBOutlet weak var overwatchView: UIView!
+    @IBOutlet weak var pokemonView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        lolView.layer.cornerRadius = 10
+        bagView.layer.cornerRadius = 10
+        overwatchView.layer.cornerRadius = 10
+        pokemonView.layer.cornerRadius = 10
+        
+        lolView.setShadow(color: UIColor.black.cgColor, width: 1, height: 1, opacity: 0.1, radius: 1.5)
+        bagView.setShadow(color: UIColor.black.cgColor, width: 1, height: 1, opacity: 0.1, radius: 1.5)
+        overwatchView.setShadow(color: UIColor.black.cgColor, width: 1, height: 1, opacity: 0.1, radius: 1.5)
+        pokemonView.setShadow(color: UIColor.black.cgColor, width: 1, height: 1, opacity: 0.1, radius: 3)
     }
 
 }
